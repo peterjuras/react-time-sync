220eec0ba81004fce4b87394977eb8525c2ecafb
Peter Juras <me@peterjuras.com>
2018-11-01 11:53:02 +0100

    Export useTime and useCountdown hooks in index

--- src/index.js
+++ src/index.js
@@ -4,4 +4,6 @@ export { connectTime } from "./connect-time";
 export { default as TimeProvider } from "./time-provider";
 export { default as Timed } from "./timed";
 export { default as Countdown } from "./countdown";
+export { useTime } from "./use-time";
+export { useCountdown } from "./use-countdown";
 export const { SECONDS, MINUTES, HOURS, DAYS } = TimeSync;

bc40b12f6a3271d1c2bdf0956dd6138b0487993f
Peter Juras <me@peterjuras.com>
2019-02-16 11:31:14 +0100

    Change build folder to /build

--- scripts/ensure-publish-path.js
+++ scripts/ensure-publish-path.js
@@ -2,9 +2,9 @@ const path = require("path");
 
 const splitted = process.cwd().split(path.sep);
 
-if (splitted[splitted.length - 1] !== "lib") {
+if (splitted[splitted.length - 1] !== "build") {
   console.log(
-    "ERROR! You can only publish outside of the transpiled /lib folder\n"
+    "ERROR! You can only publish outside of the transpiled /build folder\n"
   );
   process.exit(1);
 }

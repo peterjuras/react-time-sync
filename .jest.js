74ac368aa2823d93364a931b7c089b82179bfa14
Peter Juras <me@peterjuras.com>
2019-05-12 09:33:28 +0200

    build: Test TypeScript without ts-jest

--- .jest.js
+++ .jest.js
@@ -1,6 +1,5 @@
 module.exports = {
   roots: ["<rootDir>/src"],
-  preset: "ts-jest",
   collectCoverage: true,
   collectCoverageFrom: ["src/**/*.ts", "src/**/*.tsx"],
   coverageReporters: ["text"]

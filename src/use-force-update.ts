b58b7b9186227a3d6cae3076b2303522ef1ec5c8
Peter Juras <me@peterjuras.com>
2019-12-21 13:17:19 +0100

    fix: remove empty action for force update

--- src/use-force-update.ts
+++ src/use-force-update.ts
@@ -1,8 +1,6 @@
 import { useReducer } from "react";
 
-const EMPTY_ACTION = {};
-
 export function useForceUpdate(): () => void {
   const [, forceUpdate] = useReducer((x): number => x + 1, 0);
-  return (): void => forceUpdate(EMPTY_ACTION);
+  return (): void => forceUpdate();
 }
